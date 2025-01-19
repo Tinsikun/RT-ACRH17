d3ccac66c812773750a48aa83246a38ae0ae8257
Tinsikun <168881852+Tinsikun@users.noreply.github.com>
2025-01-19 14:12:19 +0800

    Create diy-part2.sh

---
+++ diy-part2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify default IP 修改默认网关 '2024/7/15
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+# Modify aliyundrive-webdav
+echo 'start modify aliyundrive-webdav Makefile'
+sed -i 's/stripped/release/g' feeds/packages/multimedia/aliyundrive-webdav/Makefile
+echo 'modify aliyundrive-webdav Makefile OK'
+
+# Modify hostname 修改主机名
+sed -i 's/OpenWrt/RT-ACRH17/g' package/base-files/files/bin/config_generate
+
+# Modify default theme 修改默认主题
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
